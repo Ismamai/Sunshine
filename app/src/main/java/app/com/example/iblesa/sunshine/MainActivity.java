95ee249afbc4b0d9b58b50059940f2ae1f3b4f0b
Ismael Blesa <iblesa@Ismaels-MacBook-Pro.local>
2015-02-18 23:28:56 +0000

    Opening the Map implicit intent

--- app/src/main/java/app/com/example/iblesa/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/iblesa/sunshine/MainActivity.java
@@ -1,14 +1,18 @@
 package app.com.example.iblesa.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +44,25 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+        if (id == R.id.action_preferred_location) {
+            openPreferredLocationInMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String location = pref.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri geo = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", location).build();
+        intent.setData(geo);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Could not call " + location + " and geolocation "+geo);
+        }
     }
 
 }
