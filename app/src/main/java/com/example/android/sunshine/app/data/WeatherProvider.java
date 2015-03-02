f2e8f03f99830ede064824e42923d7bd7f01f07d
Ismael Blesa <iblesa@Ismaels-MacBook-Pro.local>
2015-03-02 23:22:51 +0000

    Adding logic to define UriMatchers

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -111,13 +111,19 @@ public class WeatherProvider extends ContentProvider {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
 
+          final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER, WEATHER);
+
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_LOCATION, LOCATION);
 
 
         // 3) Return the new matcher!
-        return null;
+        return sURIMatcher;
     }
 
     /*
