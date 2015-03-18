3c03a09180cd8fc4206145a1f8761fe43f3991b1
Ismael Blesa <iblesa@Ismaels-MacBook-Pro.local>
2015-03-18 19:22:44 +0000

    Create list_item_forecast layout and item_forecast_today

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -62,8 +62,8 @@ public class ForecastAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
-
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+//Change for now until new layout is correclty linked
+//        TextView tv = (TextView)view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
